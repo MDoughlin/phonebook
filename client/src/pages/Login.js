db6431324016a904cb6f6b481e237c9e61eea008
Micheline <MDoughlin@hotmail.com>
2023-09-20 23:47:37 -0400

    Completed Login and Register

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,10 +1,27 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Login = () => {
+
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+
+    setCredentials({ ...credentials, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <>
       <h3>Login </h3>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div class="form-group">
           <label for="emailInput" class="form-label mt-4">Email address</label>
           <input
@@ -13,17 +30,25 @@ const Login = () => {
             id="emailInput"
             aria-describedby="emailHelp"
             name="email"
+            value={credentials.email}
+            onChange={handleInputChange}
             placeholder="Enter email"
+            required
           />
         </div>
-        <div class="form-group">
-          <label for="passwordlInput" class="form-label mt-4">Password</label>
+        <div className="form-group">
+          <label htmlFor="passwordInput" className="form-label mt-4">
+            Password
+          </label>
           <input
             type="password"
-            class="form-control"
+            className="form-control"
             id="passwordInput"
-            name="email"
-            placeholder="Enter password"
+            name="password"
+            value={credentials.password}
+            onChange={handleInputChange}
+            placeholder="Enter Password"
+            required
           />
         </div>
         <input type="submit" value="Login" className="btn btn-primary m-3" />
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,7 +1,69 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 const Register = () => {
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleInputChange = event => {
+    const { name, value } = event.target;
+
+    setCredentials({ ...credentials, [name]: value });
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+  };
   return (
     <>
-      This is register
+      <h3>Create account </h3>
+      <form onSubmit={handleSubmit}>
+        <div class="form-group">
+          <label for="emailInput" class="form-label mt-4">Email address</label>
+          <input
+            type="email"
+            class="form-control"
+            id="emailInput"
+            aria-describedby="emailHelp"
+            name="email"
+            value={credentials.email}
+            onChange={handleInputChange}
+            placeholder="Enter email"
+            required
+          />
+        </div>
+        <div class="form-group">
+          <label for="passwordlInput" class="form-label mt-4">Password</label>
+          <input
+            type="password"
+            class="form-control"
+            id="passwordInput"
+            name="password"
+            value={credentials.password}
+            onChange={handleInputChange}
+            placeholder="Enter password"
+            required
+          />
+        </div>
+        <div class="form-group">
+          <label for="passwordlInput" class="form-label mt-4">Confirm Password</label>
+          <input
+            type="password"
+            class="form-control"
+            id="confirmPassword"
+            name="confirmPassword"
+            value={credentials.confirmPassword}
+            onChange={handleInputChange}
+            placeholder="Confirm password"
+            required
+          />
+        </div>
+        <input type="submit" value="register" className="btn btn-primary m-3" />
+        <p> Already have an account ? <Link to="/login">Login</Link></p>
+      </form>
     </>
   );
 };
