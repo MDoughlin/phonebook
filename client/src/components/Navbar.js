e6e01a648929f69f1f1aaa0074a5c8f1f603b372
Micheline <MDoughlin@hotmail.com>
2023-09-20 22:27:27 -0400

    Login page complete

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,19 +1,26 @@
+import { Link } from "react-router-dom";
+
 const Navbar = ({ title = "Contact List" }) => {
   return (
     <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">{title}</a>
+        <Link to="/">
+          <a class="navbar-brand">{title}</a>
+        </Link>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarColor01">
           <ul class="navbar-nav ms-auto">
             <li class="nav-item">
-              <a class="nav-link active" href="#">Login
-              </a>
+              <Link to="/login">
+                <a class="nav-link active">Login</a>
+              </Link>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Register</a>
+              <Link to="/register">
+                <a class="nav-link" href="#">Register</a>
+              </Link>
             </li>
           </ul>
         </div>
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,34 @@
+import { Link } from "react-router-dom";
+
 const Login = () => {
   return (
     <>
-      This is login
+      <h3>Login </h3>
+      <form>
+        <div class="form-group">
+          <label for="emailInput" class="form-label mt-4">Email address</label>
+          <input
+            type="email"
+            class="form-control"
+            id="emailInput"
+            aria-describedby="emailHelp"
+            name="email"
+            placeholder="Enter email"
+          />
+        </div>
+        <div class="form-group">
+          <label for="passwordlInput" class="form-label mt-4">Password</label>
+          <input
+            type="password"
+            class="form-control"
+            id="passwordInput"
+            name="email"
+            placeholder="Enter password"
+          />
+        </div>
+        <input type="submit" value="Login" className="btn btn-primary m-3" />
+        <p> Don't have an account ? <Link to="/register">Create one</Link></p>
+      </form>
     </>
   );
 };
